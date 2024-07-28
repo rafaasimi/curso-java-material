b66c6b4efa42b8b6a509f016e902f970ea79bbba
Rafael Simionato <rafaasimi@hotmail.com>
2024-07-28 09:12:24 -0300

    exercicio: calcular idade em dias

---
+++ conhecendo_a_base/exercicios/src/main/java/DiasDeIdade/DiasDeIdade.java
@@ -0,0 +1,32 @@
+package DiasDeIdade;
+
+import java.util.Scanner;
+
+/*
+ * Escreva um programa que leia uma idade em anos e imprima essa idade em dias.
+ *
+ * Exemplo: 1 ano -> 365 dias de idade.
+ */
+public class DiasDeIdade {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Vamos calcular quantos dias você já viveu?");
+
+        System.out.println("Qual a sua idade em anos? (Exemplo: 27)");
+        int idade = scanner.nextInt();
+
+        scanner.close();
+
+        calcularDiasDeIdade(idade);
+    }
+
+    static public void calcularDiasDeIdade(int idade) {
+        int idadeEmDias = idade * 365;
+
+        System.out.printf("Você já viveu %d dias.", idadeEmDias);
+    }
+
+}
