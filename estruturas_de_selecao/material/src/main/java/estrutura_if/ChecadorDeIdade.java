90caf9536bf3661386ed997192413cd245c59fcb
Rafael Simionato <rafaasimi@hotmail.com>
2024-07-29 09:13:28 -0300

    aula: operador ternario if

--- estruturas_de_selecao/material/src/main/java/estrutura_if/ChecadorDeIdade.java
+++ estruturas_de_selecao/material/src/main/java/estrutura_if/ChecadorDeIdade.java
@@ -24,11 +24,14 @@ public class ChecadorDeIdade {
 //            System.out.println("Esse filme é para maiores de 18 anos.");
 //        }
 
-        if (idade >= 18)
-            System.out.println("Você pode assistir esse filme.");
-        else
-            System.out.println("Esse filme é para maiores de 18 anos.");
+//        if (idade >= 18)
+//            System.out.println("Você pode assistir esse filme.");
+//        else
+//            System.out.println("Esse filme é para maiores de 18 anos.");
 
+        // If ternário: condicao ? expressao1 : expressao2
+        String mensagem = (idade >= 18) ? "Você pode assistir esse filme." : "Esse filme é para maiores de 18 anos.";
+        System.out.println(mensagem);
 
     }
 
