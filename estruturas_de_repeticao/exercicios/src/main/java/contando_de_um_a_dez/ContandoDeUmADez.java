3a4e9a98f101a544f401f6fb83f13535664e7c4a
Rafael Simionato <rafaasimi@hotmail.com>
2024-07-30 15:36:39 -0300

    exercicio: contando de 1 a 10

---
+++ estruturas_de_repeticao/exercicios/src/main/java/contando_de_um_a_dez/ContandoDeUmADez.java
@@ -0,0 +1,22 @@
+package contando_de_um_a_dez;
+
+/*
+ * Escreva um programa Java que imprime números de 1 à 10
+ * utilizando a estrutura de repetição while.
+ * O programa deve imprimir cada número em uma
+ * linha diferente.
+ */
+public class ContandoDeUmADez {
+
+    public static void main(String[] args) {
+
+        int iteracao = 1;
+        while (iteracao <= 10) {
+            System.out.println(iteracao);
+
+            iteracao++;
+        }
+
+    }
+
+}
