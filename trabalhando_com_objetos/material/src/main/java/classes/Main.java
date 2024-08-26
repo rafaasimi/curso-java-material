4354251cc62e4ade9adf4cc6ab117b56a8d0833d
Rafael Simionato <rafaasimi@hotmail.com>
2024-08-26 09:17:49 -0300

    aula: modificador e métodos static

--- trabalhando_com_objetos/material/src/main/java/classes/Main.java
+++ trabalhando_com_objetos/material/src/main/java/classes/Main.java
@@ -6,15 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Casa casa1 = new Casa();
-        Casa casa2 = new Casa(true);
-        Casa casa3 = new Casa(true);
+//        Casa casa1 = new Casa();
+//        Casa casa2 = new Casa(true);
+//        Casa casa3 = new Casa(true);
+//
+//        System.out.println(casa1.inteligente);
+//        System.out.println(casa1.nome);
+//
+//        casa2.acenderLuz();
+//        casa3.acenderLuz();
 
-        System.out.println(casa1.inteligente);
-        System.out.println(casa1.nome);
-
-        casa2.acenderLuz();
-        casa3.acenderLuz();
+//        Double soma = new Matematica().soma(2, 5);
+        Double soma = Matematica.soma(2, 5);
+        System.out.println(soma);
 
     }
 
---
+++ trabalhando_com_objetos/material/src/main/java/classes/Matematica.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class Matematica {
+
+    public static Double soma(double numero1, double numero2) {
+        return numero1 + numero2;
+    }
+
+}
