8adebdbbe179031802f31bca930f598f9a19a5cf
Rafael Simionato <rafaasimi@hotmail.com>
2024-09-04 17:00:58 -0300

    aula: array de objetos

--- trabalhando_com_objetos/material/src/main/java/classes/Arrays.java
+++ trabalhando_com_objetos/material/src/main/java/classes/Arrays.java
@@ -18,6 +18,11 @@ public class Arrays {
         // Saída: [5, 10, 15, 20, 25, 30]
         System.out.println(java.util.Arrays.toString(idades));
 
+
+        // Arrays de Objetos
+        // Saída: [classes.Gato@eed1f14, classes.Gato@7229724f]
+        Gato[] gatos = {new Gato("Leia"), new Gato("Rey")};
+        System.out.println(java.util.Arrays.toString(gatos));
     }
 
 }
---
+++ trabalhando_com_objetos/material/src/main/java/classes/Gato.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class Gato {
+
+    public String nome;
+
+    public Gato(String nome) {
+        this.nome = nome;
+    }
+
+    public String toString() {
+        return "{ nome: " + this.nome + " }";
+    }
+
+}
