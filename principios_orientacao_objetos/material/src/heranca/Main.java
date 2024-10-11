171f34917dd308d44702864cf18a071299e85fbb
Rafael Simionato <rafaasimi@hotmail.com>
2024-10-11 08:50:09 -0300

    aula: herança - uso do instanceof

--- principios_orientacao_objetos/material/src/heranca/Main.java
+++ principios_orientacao_objetos/material/src/heranca/Main.java
@@ -10,16 +10,23 @@ public class Main {
     public static void main(String[] args) {
 
         SuperHeroi[] superHerois = {
-                new HomemAranha("Roupa vermelha com teias", new String[] {"Lançar teia"}),
-                new HomemDeFerro("Armadura de ferro vermelha", new String[] {"Voar", "Misseis"}),
-                new ViuvaNegra("Roupa preta", new String[] {"Luta"}),
+                new HomemAranha("Roupa vermelha com teias", new String[]{"Lançar teia"}),
+                new HomemDeFerro("Armadura de ferro vermelha", new String[]{"Voar", "Misseis"}),
+                new ViuvaNegra("Roupa preta", new String[]{"Luta"}),
         };
+
+        validar(superHerois);
 
         for (SuperHeroi superHeroi : superHerois) {
             superHeroi.usarSuperPoder(0);
             System.out.println(superHeroi.getTraje());
         }
+    }
 
+    static void validar(SuperHeroi[] superHerois) {
+        if (superHerois[0] instanceof HomemAranha) {
+            System.out.println("Lógica de validação do Homem Aranha!");
+        }
     }
 
 }
