2f825ad797202060dd48a824a62e54b208c48bb9
Rafael Simionato <rafaasimi@hotmail.com>
2024-11-11 10:01:46 -0300

    aula: casting polimorfismo

--- principios_orientacao_objetos/material/src/polimorfismo/Cozinheiro.java
+++ principios_orientacao_objetos/material/src/polimorfismo/Cozinheiro.java
@@ -6,4 +6,8 @@ public class Cozinheiro implements Trabalhador{
     public void trabalhar() {
         System.out.println("Fazer comida.");
     }
+
+    public void temperarComida() {
+        System.out.println("Temperar comida.");
+    }
 }
--- principios_orientacao_objetos/material/src/polimorfismo/Main.java
+++ principios_orientacao_objetos/material/src/polimorfismo/Main.java
@@ -3,11 +3,25 @@ package polimorfismo;
 public class Main {
 
     public static void main(String[] args) {
-        Trabalhador programador = new Programador();
-        Trabalhador cozinheiro = new Cozinheiro();
+        Trabalhador[] trabalhadores = {new Programador(), new Cozinheiro()};
+//        Trabalhador programador = new Programador();
+//        Trabalhador cozinheiro = new Cozinheiro();
 
-        programador.trabalhar();
-        cozinheiro.trabalhar();
+//        programador.trabalhar();
+//        cozinheiro.trabalhar();
+
+        trabalhar(trabalhadores);
+
+    }
+
+    private static void trabalhar(Trabalhador[] trabalhadores) {
+        for(Trabalhador trabalhador : trabalhadores) {
+            trabalhador.trabalhar();
+
+            if(trabalhador instanceof Cozinheiro) {
+                ((Cozinheiro) trabalhador).temperarComida();
+            }
+        }
     }
 
 }
