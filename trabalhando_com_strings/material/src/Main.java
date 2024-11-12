09830efb439a61c7978bceaa03533fcb64ff5dae
Rafael Simionato <rafaasimi@hotmail.com>
2024-11-12 09:45:35 -0300

    aula: imutabilidade das strings

---
+++ trabalhando_com_strings/material/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String texto1 = "Texto customizado";
+        String texto2 = "Texto customizado";
+        String outroTexto = new String("Texto customizado");
+
+        System.out.println(texto1);
+        System.out.println(outroTexto);
+
+        System.out.println(texto1 == outroTexto);
+        // Saída: false (pois é comparado o endereço de mémória)
+
+        System.out.println(texto1.equals(outroTexto));
+        // Saída: true (pois é comparado o valor dessa variavel)
+
+        System.out.println(texto1 == texto2);
+        // Saída: true (pois o conteúdo são iguais e o Java aproveita o valor do pool de strings)
+    }
+}
