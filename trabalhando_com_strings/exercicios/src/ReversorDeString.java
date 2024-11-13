8fde952104593e0d59f832b97694847b365e879d
Rafael Simionato <rafaasimi@hotmail.com>
2024-11-13 15:34:28 -0300

    exercicio: reverter string

---
+++ trabalhando_com_strings/exercicios/src/ReversorDeString.java
@@ -0,0 +1,21 @@
+public class ReversorDeString {
+
+    public static void main(String[] args) {
+
+        System.out.println(reverter("Olá, Mundo!"));
+
+    }
+
+    static String reverter(String string) {
+
+            String stringReversa = "";
+
+            for(int i = string.length() - 1; i >= 0; i--) {
+                stringReversa += string.charAt(i);
+            }
+
+            return stringReversa;
+
+    }
+
+}
