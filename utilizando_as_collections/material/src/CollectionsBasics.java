7afbaabeceb1a09bb6b86c1a9d7eb10ca8324553
Rafael Simionato <rafaasimi@hotmail.com>
2024-12-02 09:39:09 -0300

    aula: arrays vs collections

---
+++ utilizando_as_collections/material/src/CollectionsBasics.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class CollectionsBasics {
+
+    public static void main(String[] args) {
+
+//        String[] nomes = new String[3];
+        String[] nomes = {"Rafael", "Pedro", "Luiz"};
+
+        ArrayList<String> nomesArraylist = new ArrayList<>();
+
+    }
+
+}
