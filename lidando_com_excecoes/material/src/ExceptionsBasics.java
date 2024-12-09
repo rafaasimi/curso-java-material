2775c9c00617365d390d5230da8a0385505aecce
Rafael Simionato <rafaasimi@hotmail.com>
2024-12-09 09:39:34 -0300

    aula: lançando exceções com throw

--- lidando_com_excecoes/material/src/ExceptionsBasics.java
+++ lidando_com_excecoes/material/src/ExceptionsBasics.java
@@ -3,13 +3,21 @@ import java.util.List;
 
 public class ExceptionsBasics {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         List<String> nomes = new ArrayList<>();
         nomes.add("Rafael");
 
         getNomes(nomes);
+        dividir(1, 0);
+    }
 
+    private static int dividir(int i, int j) throws Exception {
+        if(j == 0) {
+           throw new Exception("Não é possível realizar divisão por zero.");
+        }
+
+        return i / j;
     }
 
     private static void getNomes(List<String> nomes) {
@@ -17,8 +25,8 @@ public class ExceptionsBasics {
 
         try {
             System.out.println(nomes.get(1));
-
             int resultado = 10/0;
+
         } catch(IndexOutOfBoundsException ex) {
             // ex.printStackTrace();
             System.out.println("O indice informado não é válido.");
